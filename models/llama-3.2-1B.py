9d27d901ebf22630d47ce797ca491e3dd1721b61
kunmo <kmjeong98@gmail.com>
2025-09-24 21:44:25 +0900

    edit llama

--- models/llama-3.2-1B.py
+++ models/llama-3.2-1B.py
@@ -3,7 +3,7 @@ import torch, transformers
 from ._hf_wrapper import HFWrapper
 ID, _SEQ = "meta-llama/Llama-3.2-1B", 32
 
-def get_model(): return HFWrapper(transformers.AutoModel.from_pretrained(ID).eval())
+def get_model(): return HFWrapper(transformers.AutoModel.from_pretrained(ID, torch_dtype=torch.bfloat16).eval())
 def get_dummy_input():
     tok = transformers.AutoTokenizer.from_pretrained(ID)
     vocab = tok.vocab_size
